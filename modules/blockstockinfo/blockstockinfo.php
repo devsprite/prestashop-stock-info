c74c180a59014ea7c89793cf565d6989098bef9a
Devsprite <dominique@chez-dominique.fr>
2016-05-29 21:24:20 +0200

    Correction des standards

--- modules/blockstockinfo/blockstockinfo.php
+++ modules/blockstockinfo/blockstockinfo.php
@@ -1,7 +1,32 @@
 <?php
+/**
+* 2007-2016 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2016 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
 
-if (!defined('_PS_VERSION_'))
+if (!defined('_PS_VERSION_')) {
     exit;
+}
 
 class Blockstockinfo extends Module
 {
@@ -9,7 +34,7 @@ class Blockstockinfo extends Module
     {
         $this->name = 'blockstockinfo';
         $this->tab = 'front_office_features';
-        $this->version = '1.0';
+        $this->version = '1.0.0';
         $this->author = 'Dominique';
         $this->need_instance = 0;
         $this->bootstrap = true;
@@ -44,22 +69,22 @@ class Blockstockinfo extends Module
 
     private function alterProductTable($method = 'add')
     {
-        $sql = $method == 'add' 
+        $sql = $method == 'add'
             ? 'ALTER TABLE ' . _DB_PREFIX_ . 'product ADD `stockinfo` VARCHAR (255) NOT NULL DEFAULT "Contactez nous pour connaître la disponibilité de ce produit"'
             : 'ALTER TABLE ' . _DB_PREFIX_ . 'product DROP COLUMN `stockinfo`';
-        
-            if (!Db::getInstance()->execute($sql)) {
-                return false;
-            }
+
+        if (!Db::getInstance()->execute($sql)) {
+            return false;
+        }
 
         return true;
     }
 
     public function hookDisplayAdminProductsExtra($params)
     {
-        $stockinfo = Db::getInstance()->getValue('SELECT stockinfo FROM '._DB_PREFIX_.'product WHERE id_product = '.(int)Tools::getValue('id_product'));
+        $stockinfo = Db::getInstance()->getValue('SELECT stockinfo FROM ' . _DB_PREFIX_ . 'product WHERE id_product = ' . (int)Tools::getValue('id_product'));
         $this->context->smarty->assign('stockinfo', $stockinfo);
-        
+
         return $this->display(__FILE__, 'adminProductsExtra.tpl');
     }
 }
--- modules/blockstockinfo/views/templates/hook/adminProductsExtra.tpl
+++ modules/blockstockinfo/views/templates/hook/adminProductsExtra.tpl
@@ -1,15 +1,40 @@
+/**
+* 2007-2016 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2016 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
 <div id="product-stockinfo" class="panel product-tab">
     <input type="hidden" name="submitted_tabs[]" value="Blockstockinfo" >
     <h3>Message d'information concernant le stock des pièces.</h3>
     <div class="form-group">
         <label for="stockinfo" class="control-label col-lg-3">Message : </label>
         <div class="col-lg-9">
-            <input type="text" name="stockinfo" value="{$stockinfo}">
+            <input type="text" name="stockinfo" value="{$stockinfo|escape:'htmlall':'UTF-8'}">
         </div>
     </div>
     <div class="panel-footer">
-        <a href="{$link->getAdminLink('AdminProducts')}" class="btn btn-default"><i class="process-icon-cancel"></i> {l s='Annuler'}</a>
-        <button type="submit" name="submitAddproduct" class="btn btn-default pull-right"><i class="process-icon-save"></i> {l s='Save'}</button>
-        <button type="submit" name="submitAddproductAndStay" class="btn btn-default pull-right"><i class="process-icon-save"></i> {l s='Save and stay'}</button>
+        <a href="{$link->getAdminLink('AdminProducts')|escape:'htmlall':'UTF-8'}" class="btn btn-default"><i class="process-icon-cancel"></i> {l s='Annuler' mod='blockstockinfo'}</a>
+        <button type="submit" name="submitAddproduct" class="btn btn-default pull-right"><i class="process-icon-save"></i> {l s='Save' mod='blockstockinfo'}</button>
+        <button type="submit" name="submitAddproductAndStay" class="btn btn-default pull-right"><i class="process-icon-save"></i> {l s='Save and stay' mod='blockstockinfo'}</button>
     </div>
 </div>
